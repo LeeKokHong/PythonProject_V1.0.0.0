04453c396c2d769f69152271e4d15e0a7a7a25dd
Brian Lee <leekokhong@gmail.com>
2024-10-18 16:26:41 +0800

    New publish to github

---
+++ 001_Beginner/2_reverseStrong.py
@@ -0,0 +1,9 @@
+s1 = "Hello World!"
+for i in range (len(s1)):
+    print (s1[len(s1) - 1 - i], end='')
+
+s1 = "Hello World!"
+for i in range(len(s1)):{
+    print(s1[len(s1) - 1 - i], end='')
+}
+print ("")
---
+++ 001_Beginner/4_game.py
@@ -0,0 +1,11 @@
+print ("Test 123")
+print ("Test 456")
+#import os
+#import random
+
+# import turtle module
+import turtle
+turtle.fd(0) # screen
+turtle.speed(0) # max speed of drawing
+turtle.bgcolor("blue")
+delay = input ("Press Enter to Finish. > ")
